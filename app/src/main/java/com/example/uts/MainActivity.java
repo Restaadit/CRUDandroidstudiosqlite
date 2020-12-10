106d59a08d3cd6b84469d4d5aa9cd9a7f32857bd
Resta Adityatama <62174559+Restaadit@users.noreply.github.com>
2020-12-10 17:59:23 +0700

    Add files via upload
    thats all is my college task rfn

---
+++ app/src/main/java/com/example/uts/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.uts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Pindah(View view) {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/uts/SecondActivity.java
@@ -0,0 +1,87 @@
+package com.example.uts;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+    EditText name, contact, dob;
+    Button insert, update, delete, view;
+    DBHelper DB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        name = findViewById(R.id.name);
+        contact = findViewById(R.id.contact);
+        dob = findViewById(R.id.dob);
+        insert = findViewById(R.id.btnInsert);
+        update = findViewById(R.id.btnUpdate);
+        delete = findViewById(R.id.btnDelete);
+        view = findViewById(R.id.btnView);
+        DB = new DBHelper(this);
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nameTXT = name.getText().toString();
+                String contactTXT = contact.getText().toString();
+                String dobTXT = dob.getText().toString();
+
+                Boolean checkinsertdata = DB.insertuserdata(nameTXT, contactTXT, dobTXT);
+                if(checkinsertdata==true)
+                    Toast.makeText(SecondActivity.this, "Input Data Berhasil", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(SecondActivity.this, "Data Belum Dimasukkan", Toast.LENGTH_SHORT).show();
+            }        });
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nameTXT = name.getText().toString();
+                String contactTXT = contact.getText().toString();
+                String dobTXT = dob.getText().toString();
+
+                Boolean checkupdatedata = DB.updateuserdata(nameTXT, contactTXT, dobTXT);
+                if(checkupdatedata==true)
+                    Toast.makeText(SecondActivity.this, "Update Data Berhasil", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(SecondActivity.this, "Data Belum Dimasukkan", Toast.LENGTH_SHORT).show();
+            }        });
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nameTXT = name.getText().toString();
+                Boolean checkudeletedata = DB.deletedata(nameTXT);
+                if(checkudeletedata==true)
+                    Toast.makeText(SecondActivity.this, "Hapus Data Berhasil", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(SecondActivity.this, "Hapus Data tidak berhasil", Toast.LENGTH_SHORT).show();
+            }        });
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor res = DB.getdata();
+                if(res.getCount()==0){
+                    Toast.makeText(SecondActivity.this, "No Entry Exists", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                StringBuffer buffer = new StringBuffer();
+                while(res.moveToNext()){
+                    buffer.append("Nama :"+res.getString(0)+"\n");
+                    buffer.append("NIM :"+res.getString(1)+"\n");
+                    buffer.append("Tanggal Lahir :"+res.getString(2)+"\n\n");
+                }
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(SecondActivity.this);
+                builder.setCancelable(true);
+                builder.setTitle("User Entries");
+                builder.setMessage(buffer.toString());
+                builder.show();
+            }        });
+    }}
